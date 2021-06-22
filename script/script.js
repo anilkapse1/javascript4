7b57b0ff56f6864ff41af49f9b4b3a30051f4588
anilkapse1 <mr.anilkapse@gmail.com>
2021-06-22 11:57:46 +0530

    second commit

--- script/script.js
+++ script/script.js
@@ -1,3 +1,18 @@
 'use strict';
 
+let circle = document.getElementById("circle");
+let upBtn = document.getElementById("upBtn");
+let downBtn = document.getElementById("downBtn");
+
+let rotateVal = circle.style.transform;
+console.log(rotateVal);
+
+let rotateSum;
+
+
+upBtn.onclick=function(){
+    rotateSum=rotateVal+'rotate(-90deg)';
+    console.log(rotateSum);
+    circle.style.transform=rotateSum;
+}
 
